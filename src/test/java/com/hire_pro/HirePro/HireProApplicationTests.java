cf68161775f286bebca0ae991c0f0d240311868c
SIRAMRULWA <sihlemrulwa@gmail.com>
2025-06-18 09:35:12 +0200

    Backend for Hire Pro is set up

---
+++ src/test/java/com/hire_pro/HirePro/HireProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hire_pro.HirePro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HireProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
